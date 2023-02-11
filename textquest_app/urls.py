97084c7c79e230532fe245698ecdf30fe36426bb
dcierra <1cebergg@mail.ru>
2023-02-11 16:07:17 +0700

    fix urls.W002 error in textquest urls

--- textquest_app/urls.py
+++ textquest_app/urls.py
@@ -3,15 +3,17 @@ from . import views
 
 
 urlpatterns = [
+    path('character-profile/<str:character_id>/', views.character_profile, name='character_profile'),
+    path('create-character/', views.create_character, name='create_character'),
+    path('edit-character/<str:character_id>/', views.edit_character, name='edit_character'),
+    path('delete-character/<str:character_id>/', views.delete_character, name='delete_character'),
+
+    path('add-location/', views.add_location, name='add_location'),
+
     path('<str:location_id>/', views.text_quest_home_page, name='text_quest_home_page'),
     path('', views.text_quest_home_page, name='text_quest_home_page'),
 
-    path('/character-profile/<str:character_id>/', views.character_profile, name='character_profile'),
-    path('/create-character/', views.create_character, name='create_character'),
-    path('/edit-character/<str:character_id>/', views.edit_character, name='edit_character'),
-    path('/delete-character/<str:character_id>/', views.delete_character, name='delete_character'),
 
-    path('/add-location/', views.add_location, name='add_location'),
     path('edit-location/<str:location_id>/', views.edit_location, name='edit_location'),
     path('delete-location/<str:location_id>/', views.delete_location, name='delete_location'),
 
